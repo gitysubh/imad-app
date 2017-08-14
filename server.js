f4b05f2fa3f4f9b5345f7de6c98465b326af371d
gitysubh <30935072+gitysubh@users.noreply.github.com>
2017-08-14 20:35:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ function createTemplate(data){
                 <link rel="stylesheet" href="ui/style.css" />
             </head>
             <body>
-                <div class="cointainer">
+                <div class="container">
                     <div>
                         <a href="/">HOME</a>
                     </div>
